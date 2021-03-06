578d253415fe850526de5e25f86c6c036fddf337
Ujjwal Arora <aroraujjwal7@gmail.com>
2021-03-06 13:55:44 -0600

    Added comments in property layer

--- PGAS-Checker/Properties.cpp
+++ PGAS-Checker/Properties.cpp
@@ -11,7 +11,14 @@ void Properties::transformState(CheckerContext &C, ProgramStateRef State) {
   C.addTransition(State);
 }
 
-
+/**
+ * @brief For a function argument or return value, the value is derived as an SVal,
+ * this function converts it into an int64_t value.
+ *
+ * @param C
+ * @param State
+ * @return int64_t
+ */
 int64_t getIntegerValueForArgument(SVal s){
   
   int64_t val = -1; // Will hold value of SVal
@@ -56,7 +63,7 @@ ProgramStateRef Properties::removeFromUnitializedList(ProgramStateRef State,
 }
 
 /**
- * @brief
+ * @brief remove this variable from the list of freed variables
  *
  * @param State
  * @param variable
@@ -91,6 +98,15 @@ ProgramStateRef Properties::freeThisAllocation(ProgramStateRef State,
   return NULL;
 }
 
+
+/**
+ * @brief add to the free list
+ *
+ * @param State
+ * @param arrayRegion
+ * @param errorNode
+ * @return ProgramStateRef
+ */
 ProgramStateRef Properties::recordThisAllocation(ProgramStateRef State,
                                                   const MemRegion* arrayRegion, ExplodedNode *errorNode) {
   RangeClass rangeClass(errorNode);
@@ -125,7 +141,7 @@ ProgramStateRef Properties::markAsUnsynchronized(ProgramStateRef State,
 }
 
 /**
- * @brief
+ * @brief mark the variable as synchronized
  *
  * @param State
  * @param variable
@@ -137,6 +153,14 @@ ProgramStateRef Properties::markAsSynchronized(ProgramStateRef State,
   return State;
 }
 
+
+/**
+ * @brief initialise an entry for arrayRegion in the path sensitive map 'RegionTracker'
+ *
+ * @param State
+ * @param arrayRegion
+ * @return ProgramStateRef
+ */
 ProgramStateRef Properties::addToArrayList(ProgramStateRef State,
                                           const MemRegion* arrayRegion) {
   
@@ -150,6 +174,18 @@ ProgramStateRef Properties::addToArrayList(ProgramStateRef State,
   return State;
 }
 
+/**
+ * @brief Mark the elements in the array denoted by arrayRegion based on a range. The range starts from 'startIndex' 
+ * and ends at 'startIndex+numElements'. An arrayRegion has a copy across several instances, the exact instance whose
+ * elements are marked is decided by the nodeIndex parameter.
+ *
+ * @param State
+ * @param arrayRegion
+ * @param startIndex
+ * @param numElements
+ * @param nodeIndex
+ * @return ProgramStateRef
+ */
 ProgramStateRef Properties::taintArray(ProgramStateRef State,
                                           const MemRegion* arrayRegion, SVal startIndex, SVal numElements, SVal nodeIndex) {
   if((State->get<RegionTracker>()).contains(arrayRegion)){
@@ -173,12 +209,30 @@ ProgramStateRef Properties::taintArray(ProgramStateRef State,
   return State;
 }
 
+/**
+ * @brief Check if integer value for argVal is non-negative or not
+ *
+ * @param argVal
+ * @return bool
+ */
 bool Properties::isArgNonNegative(SVal argVal){
 
   int64_t allocationSize = getIntegerValueForArgument(argVal);
   return (allocationSize >= 0);
 }
 
+/**
+ * @brief Check whether any of the elements in the given range are marked. The array is denoted by arrayRegion. The range starts from 'startIndex' 
+ * and ends at 'startIndex+numElements'. An arrayRegion has a copy across several instances, the exact instance whose
+ * elements are marked is decided by the nodeIndex parameter. If even one of the elements is marked, the function returns false.
+ *
+ * @param C
+ * @param arrayRegion
+ * @param startIndex
+ * @param numElements
+ * @param nodeIndex
+ * @return bool
+ */
 bool Properties::checkTrackerRange(CheckerContext &C,
                                           const MemRegion* arrayRegion, SVal startIndex, SVal numElements, SVal nodeIndex) {
   
@@ -202,22 +256,51 @@ bool Properties::checkTrackerRange(CheckerContext &C,
   
 }
 
+/**
+ * @brief Check if even one of the memory allocation created using a memory allocation routine
+ * is not freed till the shmem_finalise step. If there is even one such memory region, the function returns true. 
+ *
+ * @param C
+ * @param State
+ * @return bool
+ */
 bool Properties::testMissingFree(ProgramStateRef State){
   auto it_begin = (State->get<AllocationTracker>()).begin();
   auto it_end = (State->get<AllocationTracker>()).end();
   return (it_begin != it_end);
 }
 
+/**
+ * @brief This function is meant to find the exact line of code where an allocation was made that hasn't been freed yet. 
+ *
+ * @param State
+ * @return RangeClass
+ */
 RangeClass Properties::getMissingFreeAllocation(ProgramStateRef State){
   auto it_begin = (State->get<AllocationTracker>()).begin();
   return (it_begin)->second;
 }
 
+
+/**
+ * @brief This function checks if the memory region denoted by arrayRegion, exists in the list of symmetric memory regions that we track.
+ *
+ * @param State
+ * @param arrayRegion
+ * @return bool
+ */
 bool Properties::regionExistsInMap(ProgramStateRef State,
                                           const MemRegion* arrayRegion){
   return State->contains<RegionTracker>(arrayRegion);
 }
 
+/**
+ * @brief This function checks if the memory region denoted by arrayRegion, is symmetric.
+ *
+ * @param State
+ * @param arrayRegion
+ * @return bool
+ */
 bool Properties::isMemRegionSymmetric(ProgramStateRef State, const MemRegion* arrayRegion){
   bool regionExistsInTrackingMap = regionExistsInMap(State, arrayRegion);
   bool isStaticOrGlobal = (arrayRegion->hasGlobalsOrParametersStorage());
@@ -225,19 +308,43 @@ bool Properties::isMemRegionSymmetric(ProgramStateRef State, const MemRegion* ar
   return (isStaticOrGlobal || regionExistsInTrackingMap);
 }
 
+/**
+ * @brief This function checks if the memory region denoted by arrayRegion, is eligible for reallocation, i.e. it hasn't been freed yet.
+ *
+ * @param State
+ * @param arrayRegion
+ * @return bool
+ */
 bool Properties::isEligibleForRealloc(ProgramStateRef State, const MemRegion* arrayRegion){
   return State->contains<AllocationTracker>(arrayRegion);
 }
 
+/**
+ * @brief This function checks if the shmem_call was at the right place, i.e. between shmem_init and shmem_finalise.
+ *
+ * @param State
+ * @return bool
+ */
 bool Properties::isCallAtCorrectPlace(ProgramStateRef State){
   int64_t positionIdx = State->get<InitState>();
   return (positionIdx == 1);
 }
 
+/**
+ * @brief This function sets the value for the path-sensitive variable InitState to val.
+ *
+ * @param val
+ */
 void Properties::setValueForTheState(int64_t val){
   State->set<InitState>(val);
 }
 
+/**
+ * @brief This function clears all the markings from all the tracked memory regions across all instances, and returns the new State.
+ *
+ * @param State
+ * @return State
+ */
 ProgramStateRef Properties::clearMap(ProgramStateRef State){
     llvm::ImmutableMap<const clang::ento::MemRegion*, clang::ento::TrackingClass> map = State->get<RegionTracker>();
     for(llvm::ImmutableMap<const clang::ento::MemRegion*, clang::ento::TrackingClass>::iterator i = map.begin(); i != map.end(); i++){
@@ -249,6 +356,15 @@ ProgramStateRef Properties::clearMap(ProgramStateRef State){
     return State;
 }
 
+/**
+ * @brief This function clears adds a new markings for the tracked memory regions across a particular instance. The range of the
+ * markings is defined by the pair (startIndex, startIndex+numElements). nodeIndex points to the instance whose memoryRegion, we 
+ * need to mark.
+ *
+ * @param startIndex
+ * @param numElements
+ * @param nodeIndex
+ */
 void TrackingClass::updateTracker(DefinedOrUnknownSVal startIndex, DefinedOrUnknownSVal numElements, DefinedOrUnknownSVal nodeIndex) {
 
     std::pair<DefinedOrUnknownSVal, DefinedOrUnknownSVal> p1 = std::make_pair(startIndex, numElements);
@@ -276,7 +392,17 @@ void TrackingClass::updateTracker(DefinedOrUnknownSVal startIndex, DefinedOrUnkn
     trackingMap[index] = tV;
   }
 
-  bool TrackingClass::isRangeEmpty(DefinedOrUnknownSVal startIndex, DefinedOrUnknownSVal numElements, DefinedOrUnknownSVal nodeIndex, CheckerContext &C) const{
+/**
+ * @brief This function clears checks if a subset of the tracked memory regions is marked across a particular instance. The range 
+ * we need to check is defined by the pair (startIndex, startIndex+numElements). nodeIndex points to the instance whose 
+ * memoryRegion, we need to check. If the range we are checking does not have any marked element, it return true, else false.
+ *
+ * @param startIndex
+ * @param numElements
+ * @param nodeIndex
+ * @param C
+ */
+bool TrackingClass::isRangeEmpty(DefinedOrUnknownSVal startIndex, DefinedOrUnknownSVal numElements, DefinedOrUnknownSVal nodeIndex, CheckerContext &C) const{
 
     ProgramStateRef state = C.getState();
     SValBuilder &svalBuilder = C.getSValBuilder();
@@ -317,9 +443,13 @@ void TrackingClass::updateTracker(DefinedOrUnknownSVal startIndex, DefinedOrUnkn
     }
 
     return true;
-  }
+}
 
-  void TrackingClass::clearTrackingMap(){
+/**
+ * @brief This function clears the tracking map, which contains the range for all markings for a particular memory region
+ *
+ */
+void TrackingClass::clearTrackingMap(){
     (this->trackingMap).clear();
-  }
+}
 
